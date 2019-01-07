fe06a8dc6874ec6907d94ac9a35f022b7eb71439
Lauri Himanen <lauri.himanen@aalto.fi>
2019-01-07 17:16:46 +0000

    Added test image.

--- picamera/snapshots.py
+++ picamera/snapshots.py
@@ -1,8 +1,9 @@
 from picamera import PiCamera
+import time
+import datetime
+
+ts = time.time()
+st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d_%H:%M:%S')
 
 camera = PiCamera()
-resolutions = [(1920, 1440), (1280, 960), (1024, 768), (800, 600), (640, 480)]
-
-for res in resolutions:
-    camera.resolution = res
-    camera.capture("../testimgs/resolutions/test_{}x{}.png".format(res[0], res[1]))
+camera.capture("../testimgs/resolutions/test{}.png".format(st))
