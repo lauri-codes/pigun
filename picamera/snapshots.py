7e89ccbce8e0aef43181a550774ccdd7cee535ea
Lauri Himanen <lauri.himanen@aalto.fi>
2019-01-07 17:19:37 +0000

    Hi

--- picamera/snapshots.py
+++ picamera/snapshots.py
@@ -6,4 +6,5 @@ ts = time.time()
 st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d_%H:%M:%S')
 
 camera = PiCamera()
+camera.resolution = (1280, 720)
 camera.capture("../testimgs/resolutions/test{}.png".format(st))
