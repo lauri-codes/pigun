c81784fc7274dc40b53f69ab1b4d9dcc5dc40c1b
Lauri Himanen <lauri.himanen@aalto.fi>
2022-01-19 22:21:43 +0000

    Added the new full FOV resolution together with comments.

--- bluetooth/pigun.h
+++ bluetooth/pigun.h
@@ -40,17 +40,20 @@
 #define MMAL_CAMERA_VIDEO_PORT 1
 #define MMAL_CAMERA_CAPTURE_PORT 2
 
-// camera acquisiton settings
+// Camera acquisiton settings: This captures the full FoV with the V2 camera
+// module
 #define PIGUN_CAM_X 1640
 #define PIGUN_CAM_Y 1232
 #define PIGUN_FPS 40
 
-// camera output settings - these are 1/4th of the camera acquisition
-#define PIGUN_RES_X 320 // 410 // 320
-#define PIGUN_RES_Y 180 // 308 // 180
+// Camera output settings: these are ~1/4th of the camera acquisition. The
+// vertical resolution needs to be a multiple of 16, and the horizontal
+// resolution needs to be a multiple of 32!
+#define PIGUN_RES_X 416 // 410 // 320
+#define PIGUN_RES_Y 320 // 308 // 180
 
 // total number of pixels in the buffer - has to be the product of the previous 2
-#define PIGUN_NPX 57600 // 126280 // 57600 // 230400
+#define PIGUN_NPX 133120 // 126280 // 57600 // 230400
 
 #define PI 3.14159265
 extern MMAL_PORT_T *port_prv_in1;
