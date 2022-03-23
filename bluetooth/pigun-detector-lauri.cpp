ac35582910ceffff107c0c791e75a9bc9d67650d
Lauri Himanen <lauri.himanen@gmail.com>
2022-03-23 22:47:53 +0200

    Modified peak struct.

--- bluetooth/pigun-detector-lauri.cpp
+++ bluetooth/pigun-detector-lauri.cpp
@@ -217,13 +217,13 @@ extern "C" {
 
         // Order peaks
         MatrixXf corners(2, 4);
-        //corners << 0, PIGUN_RES_X,
-            //PIGUN_RES_Y, PIGUN_RES_X,
-            //PIGUN_RES_Y, 0,
-            //0, PIGUN_RES_X;
+        corners << 0, PIGUN_RES_X,
+            PIGUN_RES_Y, PIGUN_RES_X,
+            PIGUN_RES_Y, 0,
+            0, PIGUN_RES_X;
         vector<Peak> peaks;
         for (int i = 0; i < nBlobs; ++i) {
-            Vector2f corner = corners.row(i);
+            Vector2f corner = corners.col(i);
             int minIndex = 0;
             double minDistance = PIGUN_RES_X;
             for (int j = 0; j < nBlobs; ++j) {
